f143c8b9e4f294c91e624b3013e76d62eaf5f116
Michael Spencer <32441294+michael-spencer31@users.noreply.github.com>
2022-10-04 01:41:22 -0300

    Update InputValidator.java
    Added comments

--- InputValidator.java
+++ InputValidator.java
@@ -3,15 +3,19 @@ import java.io.*;
 
 public class InputValidator{
 
+	//this function checks if the word the user enters is a valid "wordle" word 
 	public boolean ValidWord(String word){
 
+		//if the word is not long enough return false
 		if(word.length() != 5){
 			System.out.println("Not a valid word");
 			return false;
 		}
 		try{
+			//open the file that contains valid words
 			File file = new File("wordleWords.txt");
 
+			//check if the word the user enters is found in the file and return
 			return (new Scanner(file).useDelimiter("\\Z").next()).contains(word);
 		}catch(FileNotFoundException e){
 			System.out.println("An error occurred");
